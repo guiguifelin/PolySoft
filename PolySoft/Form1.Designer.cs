4b573b94a3470ad9d85cb919e17c7bb1d1986ae8
Tankafaires <devil00795888@gmail.com>
2014-02-01 00:50:15 +0100

    ADD

--- PolySoft/Form1.Designer.cs
+++ PolySoft/Form1.Designer.cs
@@ -39,9 +39,11 @@
             this.buttonDeriver = new System.Windows.Forms.Button();
             this.textFonction = new System.Windows.Forms.TextBox();
             this.tabEtude = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.menuStrip1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPoly.SuspendLayout();
+            this.tabEtude.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -124,6 +126,7 @@
             // 
             // tabEtude
             // 
+            this.tabEtude.Controls.Add(this.richTextBox1);
             this.tabEtude.Location = new System.Drawing.Point(4, 22);
             this.tabEtude.Name = "tabEtude";
             this.tabEtude.Padding = new System.Windows.Forms.Padding(3);
@@ -131,6 +134,15 @@
             this.tabEtude.TabIndex = 1;
             this.tabEtude.Text = "Etude";
             this.tabEtude.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(176, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(380, 379);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // Form1
             // 
@@ -152,6 +164,7 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPoly.ResumeLayout(false);
             this.tabPoly.PerformLayout();
+            this.tabEtude.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -169,6 +182,7 @@
         private System.Windows.Forms.Button buttonDeriver;
         private System.Windows.Forms.TextBox textFonction;
         private System.Windows.Forms.TabPage tabEtude;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
